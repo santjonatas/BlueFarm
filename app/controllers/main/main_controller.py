cdc9f7c04f53f1993672ec88ede54ee23f64fcd5
santjonatas <jonatascaina100@gmail.com>
2024-09-27 23:07:03 -0300

    add itens

--- app/controllers/main/main_controller.py
+++ app/controllers/main/main_controller.py
@@ -1,5 +1,7 @@
 from flask import current_app, flash, render_template, redirect, url_for, jsonify, request, session, Blueprint
 from flask_login import login_required, login_user, logout_user
+from decimal import Decimal
+
 
 from app.application.config.global_repositories import GlobalRepositories
 
@@ -15,7 +17,8 @@ class MainController:
         self.blueprint.add_url_rule('/main_client/', view_func=self.main_client, methods=['GET', 'POST'])
         self.blueprint.add_url_rule('/add_to_cart/', view_func=self.add_to_cart, methods=['POST'])
         self.blueprint.add_url_rule('/remove_from_cart/', view_func=self.remove_from_cart, methods=['POST'])
-        
+        ###############
+        self.blueprint.add_url_rule('/increment_item/', view_func=self.increment_item, methods=['POST'])
 
     @login_required
     def main(self) -> None:
@@ -47,27 +50,45 @@ class MainController:
     @login_required
     def add_to_cart(self):
         produto_id = request.form.get('produto_id')
-        produto = repositories.produto_repository.get(produto_id)
+        produto_entity = repositories.produto_repository.get(obj_id=produto_id)
 
         if 'carrinho' not in session:
             session['carrinho'] = []
 
-        if produto:
-            # Garantindo que o preço é um float
-            preco_produto = float(produto.preco) if produto.preco is not None else 0.0
+        if produto_entity:
+            preco_produto = Decimal(produto_entity.preco) if produto_entity.preco is not None else Decimal('0.00')
             
             for item in session['carrinho']:
-                if item['id'] == produto.id:
+                if item['id'] == produto_entity.id:
                     item['quantidade'] += 1
+                    item['preco'] = Decimal(item['preco'])  
+                    item['preco'] += Decimal(produto_entity.preco)  
                     break
             else:
                 session['carrinho'].append({
-                    'id': produto.id,
-                    'nome': produto.nome,
-                    'preco': preco_produto,  # Armazenando o preço como float
+                    'id': produto_entity.id,
+                    'nome': produto_entity.nome,
+                    'preco': preco_produto,
                     'quantidade': 1
                 })
-
         print(session['carrinho'])  
         flash('Produto adicionado ao carrinho!')
+        return redirect(url_for('main.main_client'))
+    
+    #####################################
+
+    @login_required
+    def increment_item(self):
+        produto_id = int(request.form.get('produto_id'))
+        produto_entity = repositories.produto_repository.get(obj_id=produto_id)
+
+        if 'carrinho' in session:
+            for item in session['carrinho']:
+                if item['id'] == produto_id:
+                    item['quantidade'] += 1
+                    item['preco'] = Decimal(item['preco']) 
+                    item['preco'] += Decimal(produto_entity.preco)   
+                    break
+
+        flash('Quantidade incrementada!')
         return redirect(url_for('main.main_client'))
