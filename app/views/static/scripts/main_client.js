7049d51a582818dd1e27b72d2201565617885d44
santjonatas <jonatascaina100@gmail.com>
2024-10-06 18:35:40 -0300

    boa

--- app/views/static/scripts/main_client.js
+++ app/views/static/scripts/main_client.js
@@ -40,3 +40,99 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const botoes_sidebar = document.querySelectorAll('.botao-sidebar');
+    
+    botoes_sidebar.forEach(function(botao) {
+        botao.addEventListener('mouseover', function() {
+            botao.style.backgroundColor = '#ffffff21'; 
+            botao.style.boxshadow = '0px 0px 15px green';
+        });
+        
+        botao.addEventListener('mouseout', function() {
+            botao.style.backgroundColor = '';  
+            botao.style.boxshadow = ''
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const botao_fazer_pedido = document.querySelector('.form-bt-fazer-pedido');
+    
+    botao_fazer_pedido.addEventListener('mouseover', function() {
+        botao_fazer_pedido.style.backgroundColor = '#b3b0da'; 
+        botao_fazer_pedido.style.boxShadow = '0px 0px 10px green';  
+        botao_fazer_pedido.style.borderRadius = '5px';   
+    });
+    
+    botao_fazer_pedido.addEventListener('mouseout', function() {
+        botao_fazer_pedido.style.backgroundColor = '';  
+        botao_fazer_pedido.style.boxShadow = '';  
+        botao_fazer_pedido.style.borderRadius = ''; 
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const botoes_item_carrinho = document.querySelectorAll('.card-produto-carrinho');
+    
+    botoes_item_carrinho.forEach(function(botao_item_carrinho) {
+        botao_item_carrinho.addEventListener('mouseover', function() {
+            botao_item_carrinho.style.backgroundColor = '#ffffff21'; 
+            botao_item_carrinho.style.boxshadow = '0px 0px 15px green';
+        });
+        
+        botao_item_carrinho.addEventListener('mouseout', function() {
+            botao_item_carrinho.style.backgroundColor = '';  
+            botao_item_carrinho.style.boxshadow = ''
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const botoes_card_produto = document.querySelectorAll('.add-to-cart');
+    
+    botoes_card_produto.forEach(function(botao_card_produto) {
+        botao_card_produto.addEventListener('mouseover', function() {
+            botao_card_produto.style.backgroundColor = 'rgb(179, 179, 179)'; 
+            botao_card_produto.style.boxshadow = '0px 0px 15px green';
+        });
+        
+        botao_card_produto.addEventListener('mouseout', function() {
+            botao_card_produto.style.backgroundColor = '';  
+            botao_card_produto.style.boxshadow = ''
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const itens_carrinho = document.querySelectorAll('.bt-item-carrinho');
+    
+    itens_carrinho.forEach(function(item_carrinho) {
+        item_carrinho.addEventListener('mouseover', function() {
+            item_carrinho.style.backgroundColor = 'rgb(179, 179, 179)'; 
+            item_carrinho.style.boxshadow = '0px 0px 15px green';
+        });
+        
+        item_carrinho.addEventListener('mouseout', function() {
+            item_carrinho.style.backgroundColor = '';  
+            item_carrinho.style.boxshadow = ''
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const botoes_pagar_pedido = document.querySelectorAll('.bt-pagar-pedido');
+    
+    botoes_pagar_pedido.forEach(function(botao_pagar_pedido) {
+        botao_pagar_pedido.addEventListener('mouseover', function() {
+            botao_pagar_pedido.style.backgroundColor = 'rgb(0, 94, 0)'; 
+            botao_pagar_pedido.style.boxshadow = '0px 0px 10px green';
+        });
+        
+        botao_pagar_pedido.addEventListener('mouseout', function() {
+            botao_pagar_pedido.style.backgroundColor = '';  
+            botao_pagar_pedido.style.boxshadow = ''
+        });
+    });
+});
