f56b0a3fe30109db61380b035082899a7936f460
santjonatas <jonatascaina100@gmail.com>
2024-11-01 12:52:40 -0300

    registro voltar

--- app/views/static/scripts/register.js
+++ app/views/static/scripts/register.js
@@ -29,3 +29,19 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.bt-voltar');
+    
+    cards.forEach(function(card) {
+        card.addEventListener('mouseover', function() {
+            card.style.background = 'linear-gradient(to right bottom, #0f1413, #1d1e20, #363f2f)'; 
+            card.style.boxshadow = '';
+        });
+        
+        card.addEventListener('mouseout', function() {
+            card.style.background = 'linear-gradient(to right bottom, #151C1A, #27282C, #47533E)';  
+            card.style.boxshadow = '';
+        });
+    });
+});
