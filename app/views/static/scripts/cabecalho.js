d0cc7580e1a67142ed0bc043a218250ec02be0b0
santjonatas <jonatascaina100@gmail.com>
2024-11-01 21:53:23 -0300

    bt voltar

--- app/views/static/scripts/cabecalho.js
+++ app/views/static/scripts/cabecalho.js
@@ -55,3 +55,19 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.bt-cabecalho');
+    
+    cards.forEach(function(card) {
+        card.addEventListener('mouseover', function() {
+            card.style.background = 'linear-gradient(to right bottom, #0f1413, #1d1e20, #363f2f)'; 
+            card.style.boxshadow = '';
+        });
+        
+        card.addEventListener('mouseout', function() {
+            card.style.background = 'linear-gradient(to right bottom, #151C1A, #27282C, #47533E)';  
+            card.style.boxshadow = '';
+        });
+    });
+});
